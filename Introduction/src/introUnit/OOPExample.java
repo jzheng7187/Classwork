a69e4998ce65dcb1b02bcc7e52b77febb0c7792e
Student 6 <Student 6@10.8.33.102>
2016-09-16 12:33:51 -0400

    Added more detail

---
+++ Introduction/src/introUnit/OOPExample.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package introUnit;
+
+/**
+ * @author Student 6
+ * This class is designed to contrast with the 
+ * ProceduralExample. It embodies an Object-Oriented
+ *  approach.
+ */
+public class OOPExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//construct a student
+		Student jillian = new Senior("JIllian");
+		Student jordan = new Student("Jordan");
+		Student joseph = new Student("Joseph");
+		jillian.talk();
+		jordan.talk();
+		joseph.talk();
+		
+	}
+
+}
---
+++ Introduction/src/introUnit/Senior.java
@@ -0,0 +1,13 @@
+package introUnit;
+
+public class Senior extends Student {
+
+	public Senior(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void talk(){
+		System.out.println("I am a senior!");
+	}
+}
