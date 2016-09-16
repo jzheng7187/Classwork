4994102895c3d407fabeb20d36aa05c0a4942b49
jzheng7187 <jzheng7187@bths.edu>
2016-09-16 12:03:11 -0400

    Added a semi colon

--- Introduction/src/introUnit/ProceduralExample.java
+++ Introduction/src/introUnit/ProceduralExample.java
@@ -10,7 +10,7 @@ package introUnit;
 public class ProceduralExample {
 	public static void main(String[] args){
 		String[] names={"Jillian","Joseph","Jordan"};
-		String[] years={"Freshman","Junior","Senior"}
+		String[] years={"Freshman","Junior","Senior"};
 		System.out.println("My Name is "+names[0]+"and I am a"+years[2] );
 	}
 }
