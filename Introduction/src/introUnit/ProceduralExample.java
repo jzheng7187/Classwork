a771a5a18fc0dea851bf604b017eff2dfe0c1002
jzheng7187 <jzheng7187@bths.edu>
2016-09-16 12:03:55 -0400

    Finished statement

--- Introduction/src/introUnit/ProceduralExample.java
+++ Introduction/src/introUnit/ProceduralExample.java
@@ -11,6 +11,6 @@ public class ProceduralExample {
 	public static void main(String[] args){
 		String[] names={"Jillian","Joseph","Jordan"};
 		String[] years={"Freshman","Junior","Senior"};
-		System.out.println("My Name is "+names[0]+"and I am a"+years[2] );
+		System.out.println("My Name is "+names[0]+"and I am a"+years[2]+".");
 	}
 }
