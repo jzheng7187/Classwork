9526a2fd3b430f3c71a2282dc51c31fe435a8e10
Student 6 <Student 6@10.8.33.102>
2016-09-19 13:11:30 -0400

    1

---
+++ Introduction/src/datatype/DataTypes.java
@@ -0,0 +1,16 @@
+package datatype;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		System.out.println(5.0/2);
+		System.out.println((double) 5/2);
+		//adding the double is called casting. Changing the 5 to 5.0
+		System.out.println(5/2);
+		//gets 2.5, but rounds down
+		System.out.println(3+5.0/2+5*2);
+		System.out.println((double) 3+5/2+5*2);
+		System.out.println();
+	}
+
+}
