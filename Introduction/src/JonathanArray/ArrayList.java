73ae5e7c4936281a06496ca81735dbdeafab1ecb
Student 6 <Student 6@10.8.33.102>
2016-10-17 13:03:32 -0400

    more array

--- Introduction/src/JonathanArray/ArrayList.java
+++ Introduction/src/JonathanArray/ArrayList.java
@@ -3,17 +3,41 @@ package JonathanArray;
 public class ArrayList {
 
 	public static void main(String[] args) {
+		arrayIntoMethodRandom();
 		arrayIntoMethod();
+		rollDice();
 	}
 
 	private static void arrayIntoMethod() {
-		int [] randoms = new int[50];
-		int index = 0;
-		for(int b:randoms){
-			index ++;
-			b = (int) ((Math.random() * 50) + 1);
-			System.out.println(index + ") "+ b); 
+		int [] numbers = new int[50];
+		for(int index = 0; index < numbers.length; index++){
+			numbers[index] = (int)(index + 1);
+			System.out.println(index + ") "+ numbers[index]);
 		}
 	}
 
+	private static void arrayIntoMethodRandom() {
+		int [] randoms = new int[10];
+		int index = 0;
+		for(int b:randoms){
+			index ++;
+			b = (int) ((Math.random() * 10) + 1);
+			System.out.println(index + ") "+ b); 
+		}
+	}
+	
+	public static void rollDice(){
+		int [] roll = new int[2];
+		for(int index = 0; index < roll.length; index++){
+			roll[index] = ((int) ((Math.random() * 6) + 1)) + ((int) ((Math.random() * 6) + 1));
+			System.out.println(roll[index]);
+		}
+	}
+	
+	public static void deckOfCards(){
+		int [] cards = new int [52];
+		for(int index = 0; index < cards.length; index++){
+			
+		}
+	}
 }
