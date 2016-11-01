ac4cebd4cdfc714f0af096086e95a4e77ccad87c
Student 6 <Student 6@10.8.33.102>
2016-11-01 12:30:08 -0400

    push please

--- Introduction/src/caveExplorer/CaveExplorer.java
+++ Introduction/src/caveExplorer/CaveExplorer.java
@@ -1,10 +1,25 @@
 package caveExplorer;
 
+import java.util.Scanner;
+
 public class CaveExplorer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static caveRoom caves[][];
+	public static Scanner in;
+	public static caveRoom currentRoom;
+	public static Inventory inventory;
+	
+	public static void main(String[] args){
+		caves = new caveRoom[5][5];
+		for(int row = 0; row < caves.length; row++){
+			for(int col =0; col < caves[row].length; col++){
+				caves[row][col] = new caveRoom("This cave has coordinates" + row + "," + col);
+			}
+		}
+		currentRoom = caves[0][1];
+		currentRoom.enter();
+		//set contents of caveRoom to an x
+		caves[0][1].setConnection(0, caves[0][2], null);
+		
 	}
-
 }
