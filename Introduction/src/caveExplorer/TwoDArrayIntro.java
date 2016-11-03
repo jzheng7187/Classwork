9d2c1f0795819d17860904a913ada5347342867a
Student 6 <Student 6@10.8.33.102>
2016-11-03 12:32:41 -0400

    minesweeper

--- Introduction/src/caveExplorer/TwoDArrayIntro.java
+++ Introduction/src/caveExplorer/TwoDArrayIntro.java
@@ -5,6 +5,54 @@ import java.util.Arrays;
 public class TwoDArrayIntro {
 
 	public static void main(String[] args) {
+		boolean[][] mines = new boolean[6][6];
+		createMines(mines, 10);
+		String[][] field = new String[mines.length][mines[0].length];
+		matchValue(field, mines);
+		printPic(field);
+	}
+	
+	private static void matchValue(String[][] field, boolean[][] mines) {
+		for(int row = 0; row < field.length; row++){
+			for(int col = 0; col < field[0].length; col++){
+				if(mines[row][col] = true){
+					field[row][col] = "X";
+				}else{
+					field[row][col] = countAdjacent(mines, row, col);
+				}
+			}
+		}
+	}
+
+	private static String countAdjacent(boolean[][] mines, int r, int c) {
+		//r and c represent coordinates of element
+		//we are providing a String for
+		int count = 0;
+		//loop through row above to row below
+		for(int row = r-1; row <= r + 1; row++){
+			//loop through col left to col right
+			for(int col = c - 1; col < c + 1; col++){
+				if(row != r && col!= c){
+					
+				}
+			}
+		}
+		return null;
+	}
+
+	private static void createMines(boolean[][] mines, int numberOfMines) {
+		while(numberOfMines > 0){
+			int row = (int)(Math.random() * mines.length);
+			int col = (int)(Math.random() * mines[0].length);
+			if(mines[row][col]){
+				mines[row][col] = true;
+				numberOfMines--;
+			}
+		}
+		
+	}
+
+	public static void picDrawing(){
 		int[] arr = {0,1,2,3,4,5,6,7};
 		System.out.println(Arrays.toString(arr));
 	
@@ -91,5 +139,7 @@ public class TwoDArrayIntro {
 			System.out.println();
 		}
 	}
+	
+	
 
 }
