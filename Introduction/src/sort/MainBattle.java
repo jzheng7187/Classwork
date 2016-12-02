c16a1c4165eed6d4b07c9a67947dafa97c9a6bfe
Student 6 <Student 6@10.8.33.102>
2016-12-02 12:45:01 -0500

    Pokemon

--- Introduction/src/sort/MainBattle.java
+++ Introduction/src/sort/MainBattle.java
@@ -7,8 +7,35 @@ public class MainBattle {
 		Pokemon bulbasaur = new Pokemon("Bulbasaur", 26);
 		raticate.iChooseYou();
 		bulbasaur.iChooseYou();
-		System.out.println("Raticate is going to attack.");
-		
+		System.out.println("Raticate is preparing to attack with Super Fang.");
+		raticate.attack(bulbasaur, new Attack() {
+			
+			@Override
+			public void attack(Pokemon target) {
+				System.out.println("Super Fang is used!");
+				int newHP = target.getHP()/2;
+				target.setHp(newHP);
+			}
+		});
+		bulbasaur.attack(raticate, new Attack() {
+			
+			@Override
+			public void attack(Pokemon target) {
+				System.out.println("Poison powder is used!");
+				target.setPoisoned(true);
+				
+			}
+		});
+		//create a way for raticate and bulbasaur to attack
+		raticate.lapse();
+		bulbasaur.lapse();
+		printScore(raticate,bulbasaur);
 	}
+
+	private static void printScore(Pokemon p1, Pokemon p2) {
+		System.out.println(p1.getName() + ", HP = " + p1.getHP());
+		System.out.println(p2.getName() + ", HP = " + p2.getHP());
+	}
+	
 
 }
--- Introduction/src/sort/Pokemon.java
+++ Introduction/src/sort/Pokemon.java
@@ -7,6 +7,9 @@ public class Pokemon {
 	private String name;
 	private boolean poisoned;
 	
+	//anonymous inner type: an implementation of an interface written into a parameter
+	
+	
 	public Pokemon(String name, int level) {
 		hp = 100;
 		this.name = name;
@@ -22,6 +25,15 @@ public class Pokemon {
 
 	}
 
+	public void attack(Pokemon target, Attack attack){
+		if(Math.random() < .9){
+			attack.attack(target);
+			 System.out.println("The attack hit");
+		 }else{
+			 System.out.println("The attack missed");
+		 }
+	}
+	
 	public void iChooseYou() {
 		System.out.println("I choose you," + name + ".");
 	}
@@ -40,5 +52,12 @@ public class Pokemon {
 	
 	public int getHP(){
 		return hp;
+	}
+
+	public void lapse() {
+			if(poisoned){
+				hp -= 15;
+			}
+		
 	}
 }
