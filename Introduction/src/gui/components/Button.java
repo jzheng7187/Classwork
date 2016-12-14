77683143f78d86ec3c4c216705d7309b2341745b
Student 6 <Student 6@10.8.33.102>
2016-12-14 13:16:21 -0500

    stuffs

--- Introduction/src/gui/components/Button.java
+++ Introduction/src/gui/components/Button.java
@@ -2,6 +2,7 @@ package gui.components;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
@@ -17,17 +18,34 @@ public class Button extends TextLabel {
 		update();
 	}
 
+	public Color getColor(){
+		return color;
+	}
+	
+	public void setColor(Color c){
+		color = c;
+		update();
+	}
+	
 	@Override
 	public void update(Graphics2D g){
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		g.setColor(Color.white);
-		g.setFont(new Font(getFont(), Font.PLAIN, getSize()));
+		g.setColor(color);
+		g.fillRoundRect(0, 0, getWidth(), getHeight(), 25, 25);
 		g.setColor(Color.black);
-		g.drawRoundRect(getX(), getY(), getWidth(), getHeight(), 25, 25);
-		g.setColor(Color.blue);
-		g.fillRoundRect(getX(), getY(), getWidth(), getHeight(), 25, 25);
+		g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 25, 25);
+		g.setFont(new Font(getFont(), Font.PLAIN, getSize()));
+		FontMetrics fm = g.getFontMetrics();
 		if(getText() != null){
-			g.drawString(getText(), 4, (getHeight()-5));
+			g.setColor(Color.white);
+			String t = getText();
+			int cutoff = t.length();
+			while(cutoff > 0 && fm.stringWidth(t) > getWidth()){
+				cutoff --;
+				t = t.substring(0,cutoff); 
+			}
+			g.drawString(t, (getWidth()-fm.stringWidth(t))/2, (getHeight()+fm.getHeight()-fm.getDescent())/2);
+//			g.drawString(getText(), 4, (getHeight()-5));
 		}	
 	}	
 	
